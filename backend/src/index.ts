73f7fa194cebea11878ec94352a39bd5672e958c
Khurshidul <khurshidulalam5@gmail.com>
2023-09-27 00:03:11 +0600

    updated

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,24 +4,15 @@ import express from "express";
 import "../src/config/database";
 import cors from "cors";
 const app = express();
-const product = require("./routes/products");
+// const product = require("./routes/products");
 const material = require("./routes/paintingMaterial");
-const orders = require("./routes/order");
 const services = require("./routes/services");
-const checkout = require("./routes/payment");
 
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  }),
-);
+app.use(cors());
 // app.use(cors);
 app.use(express.json());
-app.use("/works", product);
 app.use("/materials", material);
-app.use("/orders", orders);
 app.use("/services", services);
-app.use("/checkout", checkout);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
